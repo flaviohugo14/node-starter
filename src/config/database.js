fafea79b1eb623bc94ed8d3622ac2a11fad47f1d
Flavio <flaviohugo.14@gmail.com>
2020-01-31 01:17:01 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
